9a20f84f87285ee9de95501aac1a7d93873f00be
Bombatomica_64 <qualcosa2001@gmail.com>
2023-12-16 17:25:55 +0100

    start

---
+++ so_long.h
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   so_long.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lmicheli <lmicheli@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/16 17:15:22 by lmicheli          #+#    #+#             */
+/*   Updated: 2023/12/16 17:19:38 by lmicheli         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SO_LONG_H
+# define SO_LONG_H
+
+# include "ft_printf/ft_printf.h"
+# include "ft_printf/libft.h"
+# include "ft_printf/get_next_line.h"
+
+#endif
