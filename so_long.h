513c18028cd0542176f1ae8ac11914cb41494c41
Bombatomica64 <150276837+Bombatomica64@users.noreply.github.com>
2023-12-18 19:19:07 +0100

    Lmicheli (#4)
    * map closed & letters
    
    * get_map
    
    * map_is_closed
    
    * test

--- so_long.h
+++ so_long.h
@@ -6,7 +6,7 @@
 /*   By: lmicheli <lmicheli@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/16 17:15:22 by lmicheli          #+#    #+#             */
-/*   Updated: 2023/12/18 18:18:56 by lmicheli         ###   ########.fr       */
+/*   Updated: 2023/12/18 19:13:14 by lmicheli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,6 +46,9 @@ typedef struct s_map
 	int		width;
 	int		height;
 	char	**map;
+	int		nb_c;
+	int		nb_e;
+	int		nb_p;
 }	t_map;
 
 void	*put_xmp(void *mlx, char *filename, int *width, int *height);
@@ -53,7 +56,10 @@ int		put_map(char *filename, t_data *data);
 int		wrong_letters_in_map(int fd);
 int		map_checker(char *filename);
 void	ft_error_free_map(t_data **tile, t_data **wall, void (*del)(void*));
-int		map_is_closed(int fd, int retval);
-char	**get_map(char *filename);
+int		map_is_rectangular(int fd, int retval);
+t_map	get_map(char *filename);
+int		ft_count_lines(char *filename);
+void	ft_check_if_map_is_valid(t_map map);
+int		map_is_closed(t_map map);
 
 #endif
