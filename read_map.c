4d35c137c4840278840569b551cb694da6001b24
Bombatomica_64 <qualcosa2001@gmail.com>
2023-12-18 18:19:27 +0100

    map closed & letters

--- read_map.c
+++ read_map.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   read_map.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tfalchi <tfalchi@student.42.fr>            +#+  +:+       +#+        */
+/*   By: lmicheli <lmicheli@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/18 16:41:23 by lmicheli          #+#    #+#             */
-/*   Updated: 2023/12/18 17:09:08 by tfalchi          ###   ########.fr       */
+/*   Updated: 2023/12/18 18:15:06 by lmicheli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,11 @@ int	put_map(char *filename, t_data *data)
 {
 	t_image	tile;
 	t_image	wall;
+	t_map	map;
 
 	tile.img = put_xmp(data->mlx, "assets/tile.xpm", &tile.width, &tile.height);
 	wall.img = put_xmp(data->mlx, "assets/wall.xpm", &wall.width, &wall.height);
 	if (map_checker(filename) == 0)
 		ft_error_free_map(&tile, &wall, data);
+	map.map = get_map(filename);
 }
