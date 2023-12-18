bb3e115d709a33cfb96fe58a4cc1e3d78283c1a5
Bombatomica_64 <qualcosa2001@gmail.com>
2023-12-18 17:33:27 +0100

    sesso

--- map_check.c
+++ map_check.c
@@ -6,10 +6,36 @@
 /*   By: lmicheli <lmicheli@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/18 16:55:35 by lmicheli          #+#    #+#             */
-/*   Updated: 2023/12/18 16:56:10 by lmicheli         ###   ########.fr       */
+/*   Updated: 2023/12/18 17:27:26 by lmicheli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
 
 int	map_checker(char *filename)
+{
+	int		fd;
+	t_line	line;
+	t_map	map;
+	char	*line;
+
+	map.width = 0;
+	fd = open(filename, O_RDONLY);
+	if (fd < 0)
+		return (0);
+	while (read_bytes > 0)
+	{
+		line = get_next_line(fd);
+		read_bytes = ft_strlen(line);
+		i = 0;
+		while (line[i])
+		{
+			if (line[i] != '1' && line[i] != '0' && line[i] != 'C'
+				&& line[i] != 'E' && line[i] != 'P')
+				return (0);
+			i++;
+		}
+		map.width += read_bytes;
+		free(line);
+	}
+}
--- so_long.h
+++ so_long.h
@@ -6,7 +6,7 @@
 /*   By: lmicheli <lmicheli@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/16 17:15:22 by lmicheli          #+#    #+#             */
-/*   Updated: 2023/12/18 16:54:33 by lmicheli         ###   ########.fr       */
+/*   Updated: 2023/12/18 17:33:20 by lmicheli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,9 +34,22 @@ typedef struct s_image
 	void	*img;
 }	t_image;
 
+typedef struct s_line
+{
+	char	*line;
+	int		read_bytes;
+	int		i;
+}	t_line;
+
+typedef struct s_map
+{
+	int		width;
+	int		height;
+	char	**map;
+}	t_map;
+
 void	*put_xmp(void *mlx, char *filename, int *width, int *height);
 int		put_map(char *filename, t_data *data);
 int		map_checker(char *filename);
-
 
 #endif
